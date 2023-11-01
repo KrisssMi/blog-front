282ead8f610c0f628ad21eeab477294200543006
Arkadii Parovozov <arkadii.parovozov@gmail.com>
2023-11-01 16:19:03 +0300

    First commit

---
+++ frontend/src/svg/logo.js
@@ -0,0 +1,21 @@
+function Logo() {
+  return (
+    <svg width="40" height="40" fill="url(#jsc_s_b)" viewBox="0 0 36 36">
+      <defs>
+        <linearGradient id="jsc_s_b" x1="50%" x2="50%" y1="97.078%" y2="0%">
+          <stop offset="0%" stopColor="#0062E0"></stop>
+          <stop offset="100%" stopColor="#19AFFF"></stop>
+        </linearGradient>
+      </defs>
+      <image
+        x="0"
+        y="0"
+        width="80"
+        height="80"
+        xlinkHref="../../icons/logo.png"
+      />
+    </svg>
+  );
+}
+
+export default Logo;
